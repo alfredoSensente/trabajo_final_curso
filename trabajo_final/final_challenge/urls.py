6300e04638c03a0c197fb95e8e1e848d5ee41bc9
Alfredo J. Sensente <ajsnsn93@gmail.com>
2021-09-21 11:59:51 -0600

    Se agregaron los metodos para consumir una api y se agrego un ejemplo

--- trabajo_final/final_challenge/urls.py
+++ trabajo_final/final_challenge/urls.py
@@ -1,7 +1,8 @@
 """Final challenge views"""
 from django.urls import path
-from .views import index
+from .views import index, hello_user
 
 urlpatterns = [
     path('index/', index, name='index'),
+    path('hello/', hello_user, name='hello'),
 ]
--- trabajo_final/final_challenge/views.py
+++ trabajo_final/final_challenge/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .services import get_username
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the final challenge index.")
+
+def hello_user(requests):
+    context = {
+        'name': get_username()
+    }
+    return render(requests, 'final_challenge/hello_user.html', context)

2e208be44e04a15a19fc8e78a7aed5046c6ab5e2
Alfredo J. Sensente <ajsnsn93@gmail.com>
2021-09-28 20:01:59 -0600

    Consultas base mejoradas

--- trabajo_final/final_challenge/services.py
+++ trabajo_final/final_challenge/services.py
@@ -13,7 +13,7 @@ def generate_request(url):
 
     return ERROR
 
-
+#QUERY ONE
 def get_query_one(fecha_inicio, fecha_final, anio):
     """docstring"""
     response_query_one = generate_request(
@@ -23,7 +23,7 @@ def get_query_one(fecha_inicio, fecha_final, anio):
         return response_query_one
     return ERROR
 
-
+#QUERY TWO
 def get_query_two(fecha_inicio, fecha_final, anio):
     """docstring"""
     response_query_two = generate_request(
@@ -33,7 +33,7 @@ def get_query_two(fecha_inicio, fecha_final, anio):
         return response_query_two
     return ERROR
 
-
+#QUERY THREE
 def get_query_three(fecha_inicio, fecha_final, anio):
     """docstring"""
     response_query_three = generate_request(
@@ -43,54 +43,99 @@ def get_query_three(fecha_inicio, fecha_final, anio):
         return response_query_three
     return ERROR
 
+def get_query_three_pais_cadena(fecha_inicio, fecha_final, anio, pais, cadena):
+    """docstring"""
+    response_query_three_pais_cadena = generate_request(
+        f'http://localhost:4000/api/data/getTotalPersonasCadenaPais_Pais_Cadena/{fecha_inicio}/{fecha_final}/{anio}/{pais}/{cadena}'
+    )
+    if response_query_three_pais_cadena:
+        return response_query_three_pais_cadena
+    return ERROR
 
-def get_query_four():
+def get_query_three_pais(fecha_inicio, fecha_final, anio, pais):
+    """docstring"""
+    response_query_three_pais = generate_request(
+        f'http://localhost:4000/api/data/getTotalPersonasCadenaPais_Pais/{fecha_inicio}/{fecha_final}/{anio}/{pais}'
+    )
+    if response_query_three_pais:
+        return response_query_three_pais
+    return ERROR
+
+def get_query_three_cadena(fecha_inicio, fecha_final, anio, cadena):
+    """docstring"""
+    response_query_three_cadena = generate_request(
+        f'http://localhost:4000/api/data/getTotalPersonasCadenaPais_Cadena/{fecha_inicio}/{fecha_final}/{anio}/{cadena}'
+    )
+    if response_query_three_cadena:
+        return response_query_three_cadena
+    return ERROR
+#QUERY FOUR
+def get_query_four(pelicula):
     """/api/data/getAsistenciaTotalPeliculas"""
     response_query_four = generate_request(
-        'http://localhost:4000/api/data/getAsistenciaTotalPeliculas')
+        f'http://localhost:4000/api/data/getAsistenciaTotalPeliculas/{pelicula}'
+    )
     if response_query_four:
         return response_query_four
     return ERROR
-
-
-def get_query_five():
+#QUERY FIVE
+def get_query_five(pelicula):
     """El total de personas que vieron cada pelicula en centroamerica por cadena de cine"""
     response_query_five = generate_request(
-        'http://localhost:4000/api/data/getAsistenciaCadenaPeliculas')
+        f'http://localhost:4000/api/data/getAsistenciaCadenaPeliculas/{pelicula}')
     if response_query_five:
         return response_query_five
     return ERROR
-
-
-def get_query_six():
+#QUERY SIX
+def get_query_six(pelicula):
     """El total de personas que vieron cada pelicula en centroamerica por cadena de cine"""
     response_query_six = generate_request(
-        'http://localhost:4000/api/data/getAsistenciaCadenaPeliculasPorcentaje')
+        f'http://localhost:4000/api/data/getAsistenciaCadenaPeliculasPorcentaje/{pelicula}')
     if response_query_six:
         return response_query_six
     return ERROR
-
-def get_query_seven(fecha_inicio, fecha_final, anio):
+#QUERY SEVEN
+def get_query_seven(fecha_inicio, fecha_final, anio, pelicula):
     """Mostrar para cada pelicula el total de personas que acudieron a verla en la
     semana por pais y ordenarla de la más vista a la menos vista"""
     response_query_seven = generate_request(
-        f'http://localhost:4000/api/data/getMasVistaMenosVista/{fecha_inicio}/{fecha_final}/{anio}')
+        f'http://localhost:4000/api/data/getMasVistaMenosVista/{fecha_inicio}/{fecha_final}/{anio}/{pelicula}')
     if response_query_seven:
         return response_query_seven
     return ERROR
-
-def get_query_eight(fecha_inicio, fecha_final, anio, pais):
+#QUERY EIGHT
+def get_query_eight(fecha_inicio, fecha_final, anio, pais, pelicula):
     """Total de personas por cadena de cine, por pais, por semana"""
     response_query_eight = generate_request(
-        f'http://localhost:4000/api/data/getAsistenciaCinePaisFecha/{fecha_inicio}/{fecha_final}/{anio}/{pais}')
+        f'http://localhost:4000/api/data/getAsistenciaCinePaisFecha/{fecha_inicio}/{fecha_final}/{anio}/{pais}/{pelicula}')
     if response_query_eight:
         return response_query_eight
     return ERROR
-
-def get_query_nine(fecha_inicio, fecha_final, anio, pais):
+#QUERY NINE
+def get_query_nine(fecha_inicio, fecha_final, anio, pais, pelicula):
     """Total de personas por cadena de cine, por pais, por semana en porcentaje"""
     response_query_nine = generate_request(
-        f'http://localhost:4000/api/data/getAsistenciaCinePaisPorcentaje/{fecha_inicio}/{fecha_final}/{anio}/{pais}')
+        f'http://localhost:4000/api/data/getAsistenciaCinePaisPorcentaje/{fecha_inicio}/{fecha_final}/{anio}/{pais}/{pelicula}')
     if response_query_nine:
         return response_query_nine
+    return ERROR
+
+
+#AUXILIARES
+def get_fecha_pelicula(fecha_inicio, fecha_final, anio):
+    """Muestra las peliculas que se mostraron en una semana"""
+    response_fecha_pelicula = generate_request(
+        f'http://localhost:4000/api/data/getFechaPeliculas/{fecha_inicio}/{fecha_final}/{anio}'
+    )
+    if response_fecha_pelicula:
+        return response_fecha_pelicula
+    return ERROR
+
+def get_fecha_pais_pelicula(fecha_inicio, fecha_final, anio, pais):
+    """Muestra las peliculas que se mostraron en una semana en determinado pais"""
+    response_fecha_pais_pelicula = generate_request(
+        f'http://localhost:4000/api/data/getFechaPaisPeliculas/{fecha_inicio}/{fecha_final}/{anio}/{pais}'
+    )
+    if response_fecha_pais_pelicula:
+        return response_fecha_pais_pelicula
     return ERROR
--- trabajo_final/final_challenge/views.py
+++ trabajo_final/final_challenge/views.py
@@ -4,30 +4,17 @@ from .services import get_query_one
 from .services import get_query_four
 from .services import get_query_two
 from .services import get_query_three
+from .services import get_query_three_pais_cadena
+from .services import get_query_three_pais
+from .services import get_query_three_cadena
 from .services import get_query_five
 from .services import get_query_six
 from .services import get_query_seven
 from .services import get_query_eight
 from .services import get_query_nine
 
-
-# Metodos
-
-
-def fecha_hdp(request):
-    """docstring"""
-    if request.POST['inputFechaInicio']:
-        fecha_inicio = request.POST['inputFechaInicio']
-        if request.POST['inputFechaFinal']:
-            fecha_final = request.POST['inputFechaFinal']
-            if fecha_inicio[:4] == fecha_final[:4]:
-                return True
-            else:
-                return False
-        else:
-            return False
-    else:
-        return False
+from .services import get_fecha_pelicula
+from .services import get_fecha_pais_pelicula
 
 
 # Create your views here.
@@ -35,47 +22,89 @@ def fecha_hdp(request):
 def query_one(request):
     """Formulario para ingresar los parametros para mostrar template table_one"""
     if request.method == 'POST':
-        if fecha_hdp(request) is True:
-            fecha_inicio = request.POST['inputFechaInicio']
-            fecha_final = request.POST['inputFechaFinal']
-            context = {
-                'query_one': get_query_one(
-                    fecha_inicio[-5:],
-                    fecha_final[-5:],
-                    fecha_final[:4]
-                ),
-                'fecha_inicio': fecha_inicio,
-                'fecha_final': fecha_final,
-            }
-            return render(request, 'final_challenge/templates/tables/table_one.html', context)
+        fecha_inicio = request.POST['inputFechaInicio']
+        fecha_final = request.POST['inputFechaFinal']
+        context = {
+            'query_one': get_query_one(
+                fecha_inicio[-5:],
+                fecha_final[-5:],
+                fecha_final[:4]
+            ),
+            'fecha_inicio': fecha_inicio,
+            'fecha_final': fecha_final,
+        }
+        return render(request, 'final_challenge/templates/tables/table_one.html', context)
     return render(request, 'final_challenge/templates/tables/table_one.html')
 
 
 def query_two(request):
     """Mostrar el total de personas de los 5 paises que visitaron el cine en la semana"""
     if request.method == 'POST':
-        if fecha_hdp(request) is True:
-            fecha_inicio = request.POST['inputFechaInicio']
-            fecha_final = request.POST['inputFechaFinal']
-            context = {
-                'query_two': get_query_two(
-                    fecha_inicio[-5:],
-                    fecha_final[-5:],
-                    fecha_final[:4]
-                ),
-                'fecha_inicio': fecha_inicio,
-                'fecha_final': fecha_final,
-            }
-            return render(request, 'final_challenge/templates/tables/table_two.html', context)
+        fecha_inicio = request.POST['inputFechaInicio']
+        fecha_final = request.POST['inputFechaFinal']
+        context = {
+            'query_two': get_query_two(
+                fecha_inicio[-5:],
+                fecha_final[-5:],
+                fecha_final[:4]
+            ),
+            'fecha_inicio': fecha_inicio,
+            'fecha_final': fecha_final,
+        }
+        return render(request, 'final_challenge/templates/tables/table_two.html', context)
     return render(request, 'final_challenge/templates/tables/table_two.html')
 
 
 def query_three(request):
     """Mostrar el total de personas de los 5 paises que visitaron el cine en la semana"""
     if request.method == 'POST':
-        if fecha_hdp(request) is True:
-            fecha_inicio = request.POST['inputFechaInicio']
-            fecha_final = request.POST['inputFechaFinal']
+        fecha_inicio = request.POST['inputFechaInicio']
+        fecha_final = request.POST['inputFechaFinal']
+        if (request.POST.get('inputTextPais', False) and
+            request.POST.get('inputTextCadena', False)):
+            pais = request.POST['inputTextPais']
+            cadena = request.POST['inputTextCadena']
+            context = {
+                'query_three': get_query_three_pais_cadena(
+                    fecha_inicio[-5:],
+                    fecha_final[-5:],
+                    fecha_final[:4],
+                    pais,
+                    cadena,
+                ),
+                'fecha_inicio': fecha_inicio,
+                'fecha_final': fecha_final,
+                'pais': pais,
+                'cadena': cadena,
+            }
+        elif request.POST.get('inputTextPais', False):
+            pais = request.POST['inputTextPais']
+            context = {
+                'query_three': get_query_three_pais(
+                    fecha_inicio[-5:],
+                    fecha_final[-5:],
+                    fecha_final[:4],
+                    pais,
+                ),
+                'fecha_inicio': fecha_inicio,
+                'fecha_final': fecha_final,
+                'pais': pais,
+            }
+        elif request.POST.get('inputTextCadena', False):
+            print('hola')
+            cadena = request.POST['inputTextCadena']
+            context = {
+                'query_three': get_query_three_cadena(
+                    fecha_inicio[-5:],
+                    fecha_final[-5:],
+                    fecha_final[:4],
+                    cadena,
+                ),
+                'fecha_inicio': fecha_inicio,
+                'fecha_final': fecha_final,
+                'cadena': cadena,
+            }
+        else:
             context = {
                 'query_three': get_query_three(
                     fecha_inicio[-5:],
@@ -85,49 +114,73 @@ def query_three(request):
                 'fecha_inicio': fecha_inicio,
                 'fecha_final': fecha_final,
             }
-            return render(request, 'final_challenge/templates/tables/table_three.html', context)
+        return render(request, 'final_challenge/templates/tables/table_three.html', context)
     return render(request, 'final_challenge/templates/tables/table_three.html')
 
 
-def query_four(requests):
+def query_four(request):
     """"El total de personas que vieron cada pelicula en centroamerica"""
-    context = {
-        'query_four': get_query_four()
-    }
-    return render(requests, 'final_challenge/templates/tables/table_four.html', context)
+    if request.method == 'POST':
+        pelicula = request.POST['inputTextPelicula']
+        print(pelicula)
+        context = {
+            'query_four': get_query_four(pelicula),
+            'query_five': get_query_five(pelicula),
+            'pelicula': pelicula,
+        }
+        return render(request, 'final_challenge/templates/tables/table_four.html', context)
+
+    return render(request, 'final_challenge/templates/tables/table_four.html')
 
 
 def query_five(requests):
     """El total de personas que vieron cada pelicula en centroamerica
     por cadena de cine"""
     context = {
-        'query_five': get_query_five()
+        #'query_five': get_query_five()
     }
     return render(requests, 'final_challenge/templates/tables/table_five.html', context)
 
 
-def query_six(requests):
+def query_six(request):
     """El total de personas que vieron cada pelicula en centroamerica
     por cadena de cine pero en porcentaje"""
-    context = {
-        'query_six': get_query_six()
-    }
-    print(context['query_six']['0'])
-    return render(requests, 'final_challenge/templates/tables/table_six.html', context)
+    if request.method == 'POST':
+        pelicula = request.POST['inputTextPelicula']
+        context = {
+            'query_six': get_query_six(pelicula),
+            'pelicula': pelicula
+        }
+        return render(request, 'final_challenge/templates/tables/table_six.html', context)
+    return render(request, 'final_challenge/templates/tables/table_six.html')
 
 
 def query_seven(request):
     """Mostrar para cada pelicula el total de personas que acudieron a verla en la
     semana por pais y ordenarla de la más vista a la menos vista"""
     if request.method == 'POST':
-        if fecha_hdp(request) is True:
-            fecha_inicio = request.POST['inputFechaInicio']
-            fecha_final = request.POST['inputFechaFinal']
+        fecha_inicio = request.POST['inputFechaInicio']
+        fecha_final = request.POST['inputFechaFinal']
+        if request.POST.get('inputTextPelicula', False):
+            pelicula = request.POST['inputTextPelicula']
             context = {
                 'query_seven': get_query_seven(
                     fecha_inicio[-5:],
                     fecha_final[-5:],
-                    fecha_final[:4]
+                    fecha_final[:4],
+                    pelicula
+                ),
+                'fecha_inicio': fecha_inicio,
+                'fecha_final': fecha_final,
+                'pelicula': pelicula,
+            }
+            return render(request, 'final_challenge/templates/tables/table_seven.html', context)
+        else:
+            context = {
+                'movies': get_fecha_pelicula(
+                    fecha_inicio[-5:],
+                    fecha_final[-5:],
+                    fecha_final[:4],
                 ),
                 'fecha_inicio': fecha_inicio,
                 'fecha_final': fecha_final,
@@ -139,43 +192,59 @@ def query_seven(request):
 def query_eight(request):
     """Total de personas por cadena de cine, por pais, por semana"""
     if request.method == 'POST':
-        if fecha_hdp(request) is True:
-            fecha_inicio = request.POST['inputFechaInicio']
-            fecha_final = request.POST['inputFechaFinal']
-            pais = request.POST['inputPais']
-            print(pais)
-            context = {
-                'query_eight': get_query_eight(
-                    fecha_inicio[-5:],
-                    fecha_final[-5:],
-                    fecha_final[:4],
-                    pais,
-                ),
-                'fecha_inicio': fecha_inicio,
-                'fecha_final': fecha_final,
-                'pais': pais,
-            }
-            return render(request, 'final_challenge/templates/tables/table_eight.html', context)
+        fecha_inicio = request.POST['inputFechaInicio']
+        fecha_final = request.POST['inputFechaFinal']
+        pais = request.POST['inputPais']
+        pelicula = request.POST['inputTextPelicula']
+        context = {
+            'query_eight': get_query_eight(
+                fecha_inicio[-5:],
+                fecha_final[-5:],
+                fecha_final[:4],
+                pais,
+                pelicula,
+            ),
+            'fecha_inicio': fecha_inicio,
+            'fecha_final': fecha_final,
+            'pais': pais,
+            'pelicula': pelicula,
+        }
+        return render(request, 'final_challenge/templates/tables/table_eight.html', context)
     return render(request, 'final_challenge/templates/tables/table_eight.html')
+
 
 def query_nine(request):
     """Total de personas por cadena de cine, por pais, por semana en porcentaje"""
     if request.method == 'POST':
-        if fecha_hdp(request) is True:
-            fecha_inicio = request.POST['inputFechaInicio']
-            fecha_final = request.POST['inputFechaFinal']
-            pais = request.POST['inputPais']
-            print(pais)
+        fecha_inicio = request.POST['inputFechaInicio']
+        fecha_final = request.POST['inputFechaFinal']
+        pais = request.POST['inputPais']
+        if request.POST.get('inputTextPelicula', False):
+            pelicula = request.POST['inputTextPelicula']
             context = {
                 'query_nine': get_query_nine(
                     fecha_inicio[-5:],
                     fecha_final[-5:],
                     fecha_final[:4],
                     pais,
+                    pelicula,
+                ),
+                'fecha_inicio': fecha_inicio,
+                'fecha_final': fecha_final,
+                'pais': pais,
+                'pelicula': pelicula,
+            }
+        else:
+            context = {
+                'movies': get_fecha_pais_pelicula(
+                    fecha_inicio[-5:],
+                    fecha_final[-5:],
+                    fecha_final[:4],
+                    pais,
                 ),
                 'fecha_inicio': fecha_inicio,
                 'fecha_final': fecha_final,
                 'pais': pais,
             }
-            return render(request, 'final_challenge/templates/tables/table_nine.html', context)
+        return render(request, 'final_challenge/templates/tables/table_nine.html', context)
     return render(request, 'final_challenge/templates/tables/table_nine.html')
